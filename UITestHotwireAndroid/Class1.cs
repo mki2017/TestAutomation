879233d316d76abfdf59a2065a21c99e9e5e4ed8
mohammad.iqbal <mohammad.iqbal@FL-IT08-LT>
2018-04-25 16:11:52 -0400

    Add project files.

---
+++ UITestHotwireAndroid/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.UITest;
+
+namespace UITestHotwireAndroid
+{
+    public class AppInitializer
+    {
+
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+
+                    .EnableLocalScreenshots()
+                    .ApkFile(@"C:/Users/mohammed.iqbal/Desktop/APP/HotwireFision1.2.0-debug79.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
---
+++ UITestHotwireAndroid/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace UITestHotwireAndroid
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void HotwirePostLogin()
+        {
+            app.Screenshot("First screen.");
+
+            app.WaitForElement(x => x.Id("btn_signup"), timeout: TimeSpan.FromSeconds(90));
+            app.Screenshot("OnBoard Screen with Signup button");
+            app.ScrollDown();
+            app.Tap(x => x.Id("btn_signup"));
+
+
+            app.WaitForElement(x => x.Marked("button_get_started"), timeout: TimeSpan.FromSeconds(90));
+            Assert.AreEqual("Sign Up Now", app.Query("title")[0].Text);
+            app.Screenshot("Get Started button displays");
+            app.Tap(x => x.Marked("button_get_started"));
+        }
+    }
+}
+
